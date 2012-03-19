10dc0f48d1f9939dda8f62f817c37b6d54784f64
Jorgen G. Nielsen <jgn@dbc.dk>
2012-03-19 16:56:32 +0100

    Added panel types

---
+++ panels/layouts/ding2base_twocol_2_10/ding2base-twocol-2-10.tpl.php
@@ -0,0 +1,8 @@
+<div class="panel-display omega-12-twocol-2-10" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-2 alpha">
+    <div class="inside"><?php print $content['left']; ?></div>
+  </div>
+  <div class="panel-panel grid-10 omega">
+    <div class="inside"><?php print $content['right']; ?></div>
+  </div>
+</div>
