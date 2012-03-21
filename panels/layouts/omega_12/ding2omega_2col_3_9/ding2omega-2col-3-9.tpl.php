1832bcef1715fd60ec52506089bd66ab17130c4a
Jorgen G. Nielsen <jgn@dbc.dk>
2012-03-21 12:39:20 +0100

    Added 3-9 panel layout and some css styling.

---
+++ panels/layouts/omega_12/ding2omega_2col_3_9/ding2omega-2col-3-9.tpl.php
@@ -0,0 +1,8 @@
+<div class="panel-display omega-12-twocol-3-9" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-3 alpha">
+    <div class="inside"><?php print $content['left']; ?></div>
+  </div>
+  <div class="panel-panel grid-9 omega">
+    <div class="inside"><?php print $content['right']; ?></div>
+  </div>
+</div>
